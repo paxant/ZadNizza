d797cdc0935909eb2277287a1e678e0069b7c626
paxant <s.iuzer2015@yandex.ru>
2023-07-29 19:58:15 +0700

    not commit

--- COM_PORT_SCAN.sh
+++ COM_PORT_SCAN.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-ls -l /sys/class/tty/*/device/driver | grep -v "platform/drivers/serial8250" | awk '{print $9}' | awk -F'/' '{print "/dev/" $5}' | tee ~/STM32_PC_realis/COM_PORTS.ZadNizza
+ls -l /sys/class/tty/*/device/driver | grep -v "platform/drivers/serial8250" | awk '{print $9}' | awk -F'/' '{print "" $5}'| tee ~/STM32_PC_realis/COM_PORTS.ZadNizza
 
--- main_function.cpp
+++ main_function.cpp
@@ -4,20 +4,74 @@
 #include "QSerialPortInfo"
 #include "QProcess"
 #include "QDebug"
+#include <QFile>
+#include <QByteArray>
+#include <QString>
+#include "string"
+#include "vector"
 
 #define BASH true
+#define DEBUG true
 
+/*
+DATA_STRING_COM_PORTS[] названия портов
+NUMB_PORTS их количество
+*/
+
+using namespace std;
 QSerialPort serial_port_variable;
 
 bool Definitions_device_availability()
 {
 #if BASH //work
+    //variables
     QProcess PROCESS_BASH;
-    QString BASH_COMMAND = "../STM32_PC_realis/COM_PORT_SCAN.sh";
+    QString DATA_STRING_COM_PORTS_FILE = "";
+    string DATA_STRING_COM_PORTS[100];
+    int NUMB_PORTS = 0;
+    for(int i = 0; i < 100; i++)
+        DATA_STRING_COM_PORTS[i] = "";
+    //code
+#if DEBUG
+    qDebug() << "BASH ENEBLE" ;
+#endif
+    QString BASH_COMMAND = "../STM32_PC_realis/COM_PORT_SCAN.sh"; //Получение данных о портах в файл
     PROCESS_BASH.start(BASH_COMMAND);
     PROCESS_BASH.waitForFinished();
+
+    QFile READ_A_BASH_FILE("../STM32_PC_realis/COM_PORTS.ZadNizza");
+    if (!READ_A_BASH_FILE.open(QIODevice::ReadOnly))
+        qDebug() << "FILE FAIL";
+    else
+        {
+            int NUMB = 0;
+            DATA_STRING_COM_PORTS_FILE = QString(READ_A_BASH_FILE.readAll());
+            string FILE_DATA = DATA_STRING_COM_PORTS_FILE.toStdString();
+            for(int i = 0; i < DATA_STRING_COM_PORTS_FILE.length(); i++)
+            {
+                if(FILE_DATA[i] == '\n' )
+                {
+                    i++;
+                    NUMB++;
+                }
+                DATA_STRING_COM_PORTS[NUMB] += FILE_DATA[i];
+            }
+            while(true)
+            {
+                if(DATA_STRING_COM_PORTS[NUMB_PORTS] != "" && NUMB_PORTS != 99)
+                NUMB_PORTS++;
+                else
+                    break;
+            }
+#if DEBUG
+      qDebug() << DATA_STRING_COM_PORTS;
+
+#endif
+
+    }
 #else
-     qDebug() << "BASH off" ;
+     qDebug() << "BASH OFF" ;
+     qDebug() << DATA_STRING_COM_PORTS;
 #endif
     return false;
 
